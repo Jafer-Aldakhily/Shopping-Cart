e73f173783d93553debe88e6f3104968092e9b8c
Jafer <jaferaldkly@gmail.com>
2022-02-15 15:12:25 +0200

    changing the asset to secure_asset

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -286,19 +286,19 @@
     	<div class="container">
     		<div class="row">
     			<div class="col-sm ftco-animate">
-    				<a href="#" class="partner"><img src="{{asset('frontend/images/partner-1.png')}}" class="img-fluid" alt="Colorlib Template"></a>
+    				<a href="#" class="partner"><img src="{{secure_asset('frontend/images/partner-1.png')}}" class="img-fluid" alt="Colorlib Template"></a>
     			</div>
     			<div class="col-sm ftco-animate">
-    				<a href="#" class="partner"><img src="{{asset('frontend/images/partner-2.png')}}" class="img-fluid" alt="Colorlib Template"></a>
+    				<a href="#" class="partner"><img src="{{secure_asset('frontend/images/partner-2.png')}}" class="img-fluid" alt="Colorlib Template"></a>
     			</div>
     			<div class="col-sm ftco-animate">
-    				<a href="#" class="partner"><img src="{{asset('frontend/images/partner-3.png')}}" class="img-fluid" alt="Colorlib Template"></a>
+    				<a href="#" class="partner"><img src="{{secure_asset('frontend/images/partner-3.png')}}" class="img-fluid" alt="Colorlib Template"></a>
     			</div>
     			<div class="col-sm ftco-animate">
-    				<a href="#" class="partner"><img src="{{asset('frontend/images/partner-4.png')}}" class="img-fluid" alt="Colorlib Template"></a>
+    				<a href="#" class="partner"><img src="{{secure_asset('frontend/images/partner-4.png')}}" class="img-fluid" alt="Colorlib Template"></a>
     			</div>
     			<div class="col-sm ftco-animate">
-    				<a href="#" class="partner"><img src="{{asset('frontend/images/partner-5.png')}}" class="img-fluid" alt="Colorlib Template"></a>
+    				<a href="#" class="partner"><img src="{{secure_asset('frontend/images/partner-5.png')}}" class="img-fluid" alt="Colorlib Template"></a>
     			</div>
     		</div>
     	</div>
